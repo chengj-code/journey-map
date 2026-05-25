c620d17a441acea9c8e6e4493d8d059867ea73fd
chengj-code <chengj-code@users.noreply.github.com>
2026-05-25 07:33:20 +0000

    feat: 生成项目仓库Code Wiki
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- components.d.ts
+++ components.d.ts
@@ -10,5 +10,12 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    VanCascader: typeof import('vant/es')['Cascader']
+    VanField: typeof import('vant/es')['Field']
+    VanIcon: typeof import('vant/es')['Icon']
+    VanPicker: typeof import('vant/es')['Picker']
+    VanPopup: typeof import('vant/es')['Popup']
+    VanTabbar: typeof import('vant/es')['Tabbar']
+    VanTabbarItem: typeof import('vant/es')['TabbarItem']
   }
 }
